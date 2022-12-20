b4933b069ba157ec1a0bb71a311da3689f0348bb
Sharif Rakhimov <rakhimov.sharif@gmail.com>
2022-12-20 14:44:21 -0500

    changed bucket name to generic name

--- Project 4. Streaming Redshift data to Dashboard/producing_trigger_file.py
+++ Project 4. Streaming Redshift data to Dashboard/producing_trigger_file.py
@@ -1,4 +1,4 @@
 # writing trigger file to enriched bucket for next pipeline to start
 
 with open("trigger_file.txt", 'rb') as f:
-    s3.upload_fileobj(f, "sharif-bucket2", "trigger_file.txt")
+    s3.upload_fileobj(f, "you_enriched_bucket", "trigger_file.txt")
