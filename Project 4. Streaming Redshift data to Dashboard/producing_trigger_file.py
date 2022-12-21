b24bca9301b191d8fe4fb28d76b5a47176e4a9f9
Sharif Rakhimov <rakhimov.sharif@gmail.com>
2022-12-20 22:08:04 -0500

    fixed trigger producer code

--- Project 4. Streaming Redshift data to Dashboard/producing_trigger_file.py
+++ Project 4. Streaming Redshift data to Dashboard/producing_trigger_file.py
@@ -1,4 +1,4 @@
-# writing trigger file to enriched bucket for next pipeline to start
-
-with open("trigger_file.txt", 'rb') as f:
-    s3.upload_fileobj(f, "you_enriched_bucket", "trigger_file.txt")
+# writing trigger file to enriched bucket
+trigger_file_text = "This is triger file for next pipeline (Project 4)"
+client = boto3.client('s3')
+client.put_object(Body=trigger_file_text, Bucket=trg_file_bucket.split("//")[1].split("/")[0], Key='trigger_file.txt')
