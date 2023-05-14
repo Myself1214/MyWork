fd432b88c52efcc781198b1fccdf5b2ec5e0a3c4
Sharif Rakhimov <rakhimov.sharif@gmail.com>
2023-05-14 01:20:04 -0400

    LinkedIn scraping script

---
+++ Project 1. Scraping list contact from LinkedIn/Python Scrap LinkedIn.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[15]:
+
+
+#import chrome webdriver and BeautifulSoup
+
+from bs4 import BeautifulSoup as bs
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+#declare login credentials
+username = 'rakhimov.sharif@gmail.com'
+password='1214shanya'
+
+#creating an instance of browser to work on
+browser = webdriver.Chrome('D:\All from D\chromedriver_win32\chromedriver.exe')
+
+#opening linkedIn main page to log in in browser instance opened previousely
+browser.get('https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin')
+
+#Find element (username filed) by id 
+user_filed = browser.find_element(By.ID, 'username')
+#Enter username
+user_filed.send_keys(username)
+
+#Find element (password field) by id
+pass_field= browser.find_element(By.ID, 'password')
+#Enter password
+pass_field.send_keys(password)
+
+#submit log in credentials
+pass_field.submit()
+
+
+# In[ ]:
+
+
+
+
