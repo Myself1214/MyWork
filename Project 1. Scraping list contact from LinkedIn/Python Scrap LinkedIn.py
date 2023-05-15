decd6d41e080c1315159e238e273da409e6b8c9f
Sharif Rakhimov <rakhimov.sharif@gmail.com>
2023-05-15 16:03:04 -0400

    added serach box selection

--- Project 1. Scraping list contact from LinkedIn/Python Scrap LinkedIn.py
+++ Project 1. Scraping list contact from LinkedIn/Python Scrap LinkedIn.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
 #import chrome webdriver and BeautifulSoup
 
 from bs4 import BeautifulSoup as bs
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-
+import time
 
 #declare login credentials
 username = 'your_username'
 password='your_password'
+
 
 #creating an instance of browser to work on
 browser = webdriver.Chrome('<path_to_chromedriver>\chromedriver.exe')
@@ -31,3 +32,11 @@ pass_field.send_keys(password)
 
 #submit log in credentials
 pass_field.submit()
+time.sleep(3)
+
+#Find search box
+search_box = browser.find_element(By.CLASS_NAME, 'search-global-typeahead__input')
+#Send input
+search_box.send_keys('john doe')
+search_box.send_keys(Keys.ENTER)
+time.sleep(3)
